ff38da075bb35b6930f87b6c92160c44efb88d70
Jose Ramirez <shadow661199@gmail.com>
2018-06-06 13:16:58 -0700

    data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'app';
+  heading:string ="Person Details";
+
+  person: any = {
+
+    name:{
+
+      first:'Jose',
+      last:'Noris'
+    },
+    gender:'male',
+    address:'You know'
+  };
+
+  getInitial(){
+
+    if(this.person.gender === 'male'){
+
+      return'Mr.';
+    }else if(this.person.gender ==='female'){
+      return 'Mrs.';
+    }else{
+      return '';
+    }
+  }
+
+  getAddress(){
+
+    if(this.person.address){
+
+    }
+  }
+  
+
+
 }
